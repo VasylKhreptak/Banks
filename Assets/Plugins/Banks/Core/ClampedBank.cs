27da1a3a337725dfbbc06b3640791b770032c0c0
VASYL\chrep <vasylkhreptak12@gmail.com>
2023-11-09 10:17:30 +0200

    Added New Funtionality;

--- Assets/Plugins/Banks/Core/ClampedBank.cs
+++ Assets/Plugins/Banks/Core/ClampedBank.cs
@@ -1,21 +1,23 @@
 using System;
 using Plugins.Banks.Extensions;
 using UniRx;
+using UnityEngine;
 
 namespace Plugins.Banks.Core
 {
     public abstract class ClampedBank<T> : Bank<T> where T : IComparable<T>
-
     {
         protected readonly ReactiveProperty<T> _maxValue;
         protected readonly ReactiveProperty<float> _fillAmount;
         protected readonly ReactiveProperty<T> _leftToFill;
+        public readonly IReadOnlyReactiveProperty<bool> IsFilled;
 
         public ClampedBank()
         {
             _maxValue = new ReactiveProperty<T>();
             _fillAmount = new ReactiveProperty<float>();
             _leftToFill = new ReactiveProperty<T>();
+            IsFilled = _fillAmount.Select(fillAmount => Mathf.Approximately(fillAmount, 1f)).ToReadOnlyReactiveProperty();
         }
 
         public ClampedBank(T value, T maxValue) : base(value)
@@ -27,6 +29,7 @@ namespace Plugins.Banks.Core
             _maxValue = new ReactiveProperty<T>(maxValue);
             _fillAmount = new ReactiveProperty<float>(CalculateFillAmount());
             _leftToFill = new ReactiveProperty<T>(CalculateLeftToFill());
+            IsFilled = _fillAmount.Select(fillAmount => Mathf.Approximately(fillAmount, 1f)).ToReadOnlyReactiveProperty();
         }
 
         public IReadOnlyReactiveProperty<T> MaxValue => _maxValue;
